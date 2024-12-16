c45a73d25a0e2980bfd4414943cf46d7ece22be8
Lamarck <704021302@qq.com>
2024-12-16 23:16:30 +0800

    在App.vue中添加了注释，重命名了Index组件

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <div id="app">
-    <IndexComponent></IndexComponent>
+  <div id="app">  <!--根节点，它对应于 main.js 中通过 #app 进行挂载的 DOM 节点-->
+    <IndexComponent></IndexComponent>  <!--引入了名为 IndexComponent 的子组件-->
   </div>
 </template>
 
 <script>
-import IndexComponent from "@/components/Index.vue";
+import IndexComponent from "@/components/IndexComponent.vue";  // 引入了 IndexComponent.vue 子组件
 
 export default {
   name: 'App',
