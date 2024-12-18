54e540586093853ea10ec4d34bae69a0e9d119a1
Lamarck <704021302@qq.com>
2024-12-18 15:23:51 +0800

    在Header中添加了点击方法（个人中心和退出登录）

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,20 +1,29 @@
-<script setup>
-
-</script>
-
 <template>
   <div>
   <el-dropdown>
-    <i class="el-icon-setting" style="margin-right: 15px"></i>
+    <i class="el-icon-user-solid">
+    </i><span>拉马克</span>
     <el-dropdown-menu slot="dropdown">
-      <el-dropdown-item>查看</el-dropdown-item>
-      <el-dropdown-item>新增</el-dropdown-item>
-      <el-dropdown-item>删除</el-dropdown-item>
+      <el-dropdown-item @click.native="toUser">个人中心</el-dropdown-item>
+      <el-dropdown-item @click.native="logOut">退出登录</el-dropdown-item>
     </el-dropdown-menu>
   </el-dropdown>
-  <span>拉马克</span>
   </div>
 </template>
+
+<script>
+  export default {
+    name: "HeaderComponent",
+    methods: {
+      toUser(){
+        console.log('to_user')
+      },
+      logOut(){
+        console.log('logout')
+      }
+    }
+  }
+</script>
 
 <style scoped>
 
