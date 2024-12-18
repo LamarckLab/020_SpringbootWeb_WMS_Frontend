1bec42e023aa42a6a009b13e1664e5077af1586b
Lamarck <704021302@qq.com>
2024-12-18 21:36:45 +0800

    增加了鼠标悬停事件

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <div style="display: flex;line-height: 60px">
     <div>
-      <i class="el-icon-s-fold" style="font-size: 25px" @click="collapse"></i>
+      <i class="el-icon-s-fold" style="font-size: 25px; cursor: pointer" @click="collapse"></i>
     </div>
     <div style="flex:1; text-align: center; font-size: 26px">
       <span>Welcome to Lamarck's Warehouse Management System</span>
