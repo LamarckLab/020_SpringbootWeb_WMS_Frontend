a05f0cf440f54274c9342348834e34247ceaed92
Lamarck <704021302@qq.com>
2024-12-19 11:41:02 +0800

    添加了部分注释

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,17 +1,18 @@
 <!--App.vue中<template>中只嵌入了这个MainComponent，所以实质上加载App.vue,是加载这个MainComponent-->
 
+
 <template>
-  <el-container style="height: 100%; border: 1px solid #eee; margin-top: -9px">
-    <el-aside :width="aside_width" style="background-color: rgb(238, 241, 246); height: 100%; margin-left: -9px">
+  <el-container style="height: 100%; border: 1px solid #eee; margin-top: -9px">  <!--整个页面中最大的容器-->
+    <el-aside :width="aside_width" style="background-color: rgb(238, 241, 246); height: 100%; margin-left: -9px">  <!--页面中的左侧导航栏-->
       <AsideComponent :isCollapse="isCollapse"></AsideComponent>
     </el-aside>
 
-    <el-container style="height: 100%">
-      <el-header style="text-align: right; font-size: 12px; border-bottom: darkgray 3px solid">
+    <el-container style="height: 100%">  <!--页面右侧的大容器-->
+      <el-header style="text-align: right; font-size: 12px; border-bottom: darkgray 3px solid">  <!--页面右侧的header-->
         <header-component @doCollapse="doCollapse" :icon="icon"></header-component>
       </el-header>
 
-      <el-main style="height: 100%">
+      <el-main style="height: 100%">  <!--页面右侧的数据展示部分-->
         <TableComponent></TableComponent>
       </el-main>
     </el-container>
