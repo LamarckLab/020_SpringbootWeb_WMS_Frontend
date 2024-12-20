fc681dc7ea231af180f248400da1b07f194de193
Lamarck <704021302@qq.com>
2024-12-20 22:17:05 +0800

    添加了部分注释

--- src/components/TableComponent.vue
+++ src/components/TableComponent.vue
@@ -2,7 +2,7 @@
   <div>
     <div style="margin-bottom: 10px"><!--这部分是搜素框, 里面包含name输入搜索、sex下拉选择、搜索按钮和重置按钮-->
       <el-input v-model="searchName" placeholder="Enter name please" style="width: 200px" suffix-icon="el-icon-search"
-                @keyup.enter.native="loadPost"></el-input>  <!--定义了 一个输入框-->
+                @keyup.enter.native="loadPost"></el-input>  <!--输入框，定义了提示词、长度、搜索小图标、回车事件-->
       <el-select v-model="sex" filterable placeholder="Select sex please" style="margin-left: 5px"
                  @keyup.enter.native="loadPost">
         <el-option
