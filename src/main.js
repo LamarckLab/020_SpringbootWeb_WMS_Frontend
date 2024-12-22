3dfc3995b791ad36c695017224a03493c4087f7a
Lamarck <704021302@qq.com>
2024-12-22 16:23:53 +0800

    添加了login组件，引入了router

--- src/main.js
+++ src/main.js
@@ -8,6 +8,9 @@ import 'element-ui/lib/theme-chalk/index.css';
 // 引入根组件
 import App from './App.vue'
 
+import VueRouter from "vue-router";
+import router from './router';
+
 // 导入全局样式表
 import './assets/global.css'
 
@@ -19,10 +22,13 @@ Vue.prototype.$axios=axios;
 Vue.use(ElementUI);
 // Vue.use(ElementUI,{size:'small'});
 
+Vue.use(VueRouter);
+
 // 关闭生产环境的提示
 Vue.config.productionTip = false
 
 // 定义App.vue，作为整个Vue项目的入口文件
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import VueRouter from "vue-router";
+
+const routes = [
+    {
+        path: '/',
+        name: 'login',
+        component: () => import('../components/LoginComponent'),
+    }
+]
+
+const router = new VueRouter(
+    {
+        mode:'history',
+        routes
+    }
+)
+
+export default router;
